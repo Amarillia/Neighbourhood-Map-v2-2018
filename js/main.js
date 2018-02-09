cb12d43604ad5d3a82cd7577414280655f28fe6f
Amarillia <Amarillia@users.noreply.github.com>
2018-02-09 19:39:34 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
 var map;
 
+//Create a new blank array for all the listing markers
 var markers = [];
 
-//LOAD A GOOGLE MAP
 function initMap() {
-        var derby = {lat: 52.92277, lng: -1.47663};
-        // Create a styles array to use with the map.
-        var styles=[{featureType:"road",stylers:[{hue:"#FFBB00"},{saturation:30},{lightness:30},{gamma:1}]},{featureType:"road.highway",stylers:[{hue:"#FFC200"},{saturation:-62},{lightness:46},{gamma:1}]},{featureType:"road.arterial",stylers:[{hue:"#FF0300"},{saturation:-100},{lightness:51.2},{gamma:1}]},{featureType:"road.local",stylers:[{hue:"#FF0300"},{saturation:-100},{lightness:52},{gamma:1}]},{featureType:"water",stylers:[{hue:"#747ca9"},{saturation:-20},{lightness:2.4},{gamma:1}]},{featureType:"landscape",elementType:"all",stylers:[{hue:"#FFBB00"},{saturation:43.400000000000006},{lightness:37.599999999999994},{gamma:1}]},{featureType:"landscape",elementType:"geometry.fill",stylers:[{visibility:"on"},{color:"#fdb813"},{saturation:"6"},{lightness:"65"},{gamma:"0.75"}]},{featureType:"landscape.natural.landcover",elementType:"geometry.fill",stylers:[{visibility:"on"},{hue:"#daff00"},{lightness:"99"},{saturation:"-56"}]},{featureType:"landscape.natural.terrain",elementType:"geometry.fill",stylers:[{visibility:"on"},{hue:"#c3ff00"},{saturation:"-74"},{lightness:"58"},{gamma:"4.44"},{weight:"0.92"}]},{featureType:"poi",stylers:[{hue:"#00FF6A"},{saturation:-5.1},{lightness:15.2},{gamma:1}]}],//map.setOptions({styles:styles});
-        // Constructor creates a new map - only center and zoom are required.
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: derby,
-          //maximum zoom - how much detail
+
+  var derby = {lat: 52.92277, lng: -1.47663};
+
+  // Create a styles array to use with the map.
+  var styles=[{featureType:"road",stylers:[{hue:"#FFBB00"},{saturation:30},{lightness:30},{gamma:1}]},{featureType:"road.highway",stylers:[{hue:"#FFC200"},{saturation:-62},{lightness:46},{gamma:1}]},{featureType:"road.arterial",stylers:[{hue:"#FF0300"},{saturation:-100},{lightness:51.2},{gamma:1}]},{featureType:"road.local",stylers:[{hue:"#FF0300"},{saturation:-100},{lightness:52},{gamma:1}]},{featureType:"water",stylers:[{hue:"#747ca9"},{saturation:-20},{lightness:2.4},{gamma:1}]},{featureType:"landscape",elementType:"all",stylers:[{hue:"#FFBB00"},{saturation:43.400000000000006},{lightness:37.599999999999994},{gamma:1}]},{featureType:"landscape",elementType:"geometry.fill",stylers:[{visibility:"on"},{color:"#fdb813"},{saturation:"6"},{lightness:"65"},{gamma:"0.75"}]},{featureType:"landscape.natural.landcover",elementType:"geometry.fill",stylers:[{visibility:"on"},{hue:"#daff00"},{lightness:"99"},{saturation:"-56"}]},{featureType:"landscape.natural.terrain",elementType:"geometry.fill",stylers:[{visibility:"on"},{hue:"#c3ff00"},{saturation:"-74"},{lightness:"58"},{gamma:"4.44"},{weight:"0.92"}]},{featureType:"poi",stylers:[{hue:"#00FF6A"},{saturation:-5.1},{lightness:15.2},{gamma:1}]}],//map.setOptions({styles:styles});
+
+//Constructor creates a new map - only center and zoom are required
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: derby,
+           //maximum zoom - how much detail
           zoom:12,
           //displays a pan control for panning the map
           panControl:true,
@@ -35,109 +38,106 @@ function initMap() {
           mapTypeId: google.maps.MapTypeId.ROADMAP,
           styles: styles,
 
+    });
+
+    var locations = [
+      {title: 'Derby Silk Mill', location: {lat: 52.925690, lng: -1.475733}},
+      {title: 'Derby Museum and Art Gallery', location: {lat: 52.923039, lng: -1.480145}},
+      {title: 'Markeaton Park', location: {lat: 52.933525, lng: -1.505240}},
+      {title: 'Elvaston Castle Country Park', location: {lat: 52.892102, lng: -1.387390}},
+      {title: 'Alvaston Park', location: {lat: 52.904866, lng: -1.438370}},
+      {title: 'Derby River Gardens', location: {lat: 52.922866, lng: -1.438370}}
+    ];
+
+    var largeInfowindow = new google.maps.InfoWindow();
+
+//listing marker icon
+var defaultIcon = makeMarkerIcon('0B6623');
+
+//marker icon when the user hover the marker
+var highlightedIcon = makeMarkerIcon('50C878');
+
+//Create a new lat lng balance instance which captures the southwest and northeast corners of the viewport
+    var bounds = new google.maps.LatLngBounds();
+
+    for (var i = 0; i < locations.length; i++){
+//Get the position from the location array.
+      var position = locations[i].location;
+      var title = locations[i].title;
+//Create a marker per location, and put into markers array.
+      var marker = new google.maps.Marker({
+        position: position,
+        title: title,
+        icon: defaultIcon,
+        animation: google.maps.Animation.DROP,
+        id : i 
+      });
+
+      //Push the marker to our array of markers
+      markers.push(marker);
+      //Extend the boundaries of the map for each marker
+      bounds.extend(marker.position);
+      //Create an onclick event to open an infowindow at each marker
+      marker.addListener('click', function(){
+        populateInfoWindow (this, largeInfowindow);
+      });
+
+      marker.addListener('mouseover', function(){
+        this.setIcon(highlightedIcon);
+      });
+
+      marker.addListener('mouseout', function(){
+        this.setIcon(defaultIcon);
+      });
+    }
+
+//Event listeners to show and hide the places
+    document.getElementById('show-listings').addEventListener('click', showListings);
+    document.getElementById('hide-listings').addEventListener('click', hideListings);
+
+//This function populates the infowindow when the marker is clicked.
+//We'll only allow one infowindow which will open at the marker that is clicked, and populate based on that marker's position
+    function populateInfoWindow(marker, infowindow){
+//Check to make sure the infowindow is not already opened on this marker
+      if (infowindow.marker != marker){
+        infowindow.setContent ('');
+        infowindow.marker = marker;
+        infowindow.addListener('closeclick', function(){
+          infowindow.marker = null;
         });
 
-//CREATE AN ARRAY OF MARKERS
-      // These are nearby places that will be shown to the user.
-      // Normally we'd have these in a database instead.        
-      var locations = [
-        {title: 'Derby Silk Mill', location: {lat: 52.925690, lng: -1.475733}},
-        {title: 'Derby Museum and Art Gallery', location: {lat: 52.923039, lng: -1.480145}},
-        {title: 'Markeaton Park', location: {lat: 52.933525, lng: -1.505240}},
-        {title: 'Elvaston Castle Country Park', location: {lat: 52.892102, lng: -1.387390}},
-        {title: 'Alvaston Park', location: {lat: 52.904866, lng: -1.438370}},
-        {title: 'Derby River Gardens', location: {lat: 52.922866, lng: -1.438370}}
-      ];
-      
-
-      //looping trough the locations array to create infowindows - still need to create the function populateInfowindow
-      var largeInfoWindow = new google.maps.InfoWindow();
-
-      for (i = 0; i < locations.length; i++){
-          var positions= locations[i].location;
-          var title = locations[i].title;
-
-          var marker = new google.maps.Marker({
-            map: map,
-            position: position,
-            title: title,
-            animation: google.maps.Animation.DROP,
-            icon: defaultIcon,
-            id: i
-          });
-          markers.push(marker);
-          marker.addListener('click', function(){
-            populateInfoWindow(this, largeInfoWindow);
-          })
+        infowindow.open(map, marker);
+        //Make sure the marker property is cleared if the infowindow is closed
+        
       }
-
-        //Creates a new marker for Derby
-        var marker = new google.maps.Marker({
-          position: derby,
-          map: map,
-          title: 'Derby, UK'
-        });
-
-      // This function takes in a COLOR, and then creates a new marker
-      // icon of that color. The icon will be 21 px wide by 34 high, have an origin
-      // of 0, 0 and be anchored at 10, 34).
-      function makeMarkerIcon(markerColor) {
-        var markerImage = new google.maps.MarkerImage(
-          'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
-          '|40|_|%E2%80%A2',
-          new google.maps.Size(21, 34),
-          new google.maps.Point(0, 0),
-          new google.maps.Point(10, 34),
-          new google.maps.Size(21,34));
-        return markerImage;
-      }
-
-        // The following group uses the location array to create an array of markers on initialize.
-        var highlightedIcon = makeMarkerIcon('0B6623');
-        var defaultIcon = makeMarkerIcon('50C878');
-        for (var i = 0; i < locations.length; i++) {
-          // Get the position from the location array.
-          var position = locations[i].location;
-          var title = locations[i].title;
-          // Create a marker per location, and put into markers array.
-          var marker = new google.maps.Marker({
-            map: map,
-            position: position,
-            title: title,
-            animation: google.maps.Animation.DROP,
-            icon: defaultIcon,
-            id: i
-          });
-          // Push the marker to our array of markers.
-          markers.push(marker);
-          //Change the color of the markers on hover the mouse
-          marker.addListener('mouseover', function() {
-              this.setIcon(highlightedIcon);
-          });
-          //Change the color back to the original color
-          marker.addListener('mouseout', function() {
-              this.setIcon(defaultIcon);
-          });
-      }
-
-//ALLOW THE USER TO DISPLAY AND HIDE ALL THE MARKERS ON THE MAP
-      // This function will loop through the markers array and display them all.
-      function showListings() {
+    }
+    //This function will loop through the markers array and display them all
+      function showListings (){
         var bounds = new google.maps.LatLngBounds();
-        // Extend the boundaries of the map for each marker and display the marker
-        for (var i = 0; i < markers.length; i++) {
+        //Extend the boundaries of the map for each marker and display the marker
+        for (var i = 0; i < markers.length; i++){
           markers[i].setMap(map);
           bounds.extend(markers[i].position);
         }
         map.fitBounds(bounds);
       }
-      // This function will loop through the listings and hide them all.
-      function hideListings() {
-        for (var i = 0; i < markers.length; i++) {
+
+      //This function will loop through the markers array and hide them all
+      function hideListings (){
+        for (var i = 0; i < markers.length; i++){
           markers[i].setMap(null);
         }
       }
 
-      document.getElementById('show-listings').addEventListener('click', showListings);
-      document.getElementById('hide-listings').addEventListener('click', hideListings);
+      function makeMarkerIcon(markerColor){
+        var markerImage = new google.maps.MarkerImage(
+          'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor + '|40|_|%E2%80%A2',
+          new google.maps.Size(21,34),
+          new google.maps.Point(0,0),
+          new google.maps.Point(10,34),
+          new google.maps.Size(21,34)
+          );
+        return markerImage;
+      }
 }
+
